35a73a483ef696bd8a1af07de6e5ba607337bd90
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-27 18:12:32 +0600

    comment redirect admin

--- app/my-account/dashboard/layout.jsx
+++ app/my-account/dashboard/layout.jsx
@@ -9,16 +9,16 @@ import { useSelector } from "react-redux";
 
 const DashLayout = ({ children }) => {
   const { isSidebarOpen } = useSelector((state) => state.dashboardSlice);
-  const { user } = useSelector((state) => state.authSlice);
-  const router = useRouter();
+  // const { user } = useSelector((state) => state.authSlice);
+  // const router = useRouter();
 
-  useEffect(() => {
-    if (!user || user?.uid !== process.env.NEXT_PUBLIC_ADMIN_ID) {
-      router.push("/");
-    }
+  // useEffect(() => {
+  //   if (!user || user?.uid !== process.env.NEXT_PUBLIC_ADMIN_ID) {
+  //     router.push("/");
+  //   }
 
-    return () => null;
-  }, [user]);
+  //   return () => null;
+  // }, [user]);
 
   return (
     <div className="h-screen flex fixed top-0 left-0 w-full bg-white">
